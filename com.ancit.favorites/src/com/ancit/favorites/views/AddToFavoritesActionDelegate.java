0244b75be10c55d8ec97aac6f6b9d18ca0dfee97
m.maheshbhagirath@gmail.com <Mahesh>
2022-08-18 15:41:48 +0530

    Object Actions

---
+++ com.ancit.favorites/src/com/ancit/favorites/views/AddToFavoritesActionDelegate.java
@@ -0,0 +1,47 @@
+/*
+ * FILE:            AddToFavoritesActionDelegate.java
+ *
+ * SW-COMPONENT:    com.ancit.favorites
+ *
+ * DESCRIPTION:     -
+ *
+ * COPYRIGHT:       © 2015 - 2022 Robert Bosch GmbH
+ *
+ * The reproduction, distribution and utilization of this file as
+ * well as the communication of its contents to others without express
+ * authorization is prohibited. Offenders will be held liable for the
+ * payment of damages. All rights reserved in the event of the grant
+ * of a patent, utility model or design.
+ */
+package com.ancit.favorites.views;
+
+import org.eclipse.jface.action.IAction;
+import org.eclipse.jface.dialogs.MessageDialog;
+import org.eclipse.jface.viewers.ISelection;
+import org.eclipse.ui.IObjectActionDelegate;
+import org.eclipse.ui.IWorkbenchPart;
+
+public class AddToFavoritesActionDelegate implements IObjectActionDelegate {
+
+	private IWorkbenchPart targetPart;
+
+	@Override
+	public void run(IAction action) {
+		MessageDialog.openInformation(targetPart.getSite().getShell(), "Add to Favorites",
+				"Triggered the " + getClass().getName() + " action");
+
+	}
+
+	@Override
+	public void selectionChanged(IAction action, ISelection selection) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void setActivePart(IAction action, IWorkbenchPart targetPart) {
+		this.targetPart = targetPart;
+
+	}
+
+}
