f51ca7fd41a8499d10b4e2a6ead02ea3b811b0c6
m.maheshbhagirath@gmail.com <Mahesh>
2022-08-18 16:13:25 +0530

    View Actions

---
+++ com.ancit.favorites/src/com/ancit/favorites/views/AddToFavoritesViewActionDelegate.java
@@ -0,0 +1,48 @@
+/*
+ * FILE:            AddToFavoritesViewActionDelegate.java
+ *
+ * SW-COMPONENT:    com.ancit.favorites
+ *
+ * DESCRIPTION:     -
+ *
+ * COPYRIGHT:       © 2015 - 2022 Robert Bosch GmbH
+ *
+ * The reproduction, distribution and utilization of this file as
+ * well as the communication of its contents to others without express
+ * authorization is prohibited. Offenders will be held liable for the
+ * payment of damages. All rights reserved in the event of the grant
+ * of a patent, utility model or design.
+ */
+package com.ancit.favorites.views;
+
+import org.eclipse.jface.action.IAction;
+import org.eclipse.jface.dialogs.MessageDialog;
+import org.eclipse.jface.viewers.ISelection;
+import org.eclipse.ui.IViewActionDelegate;
+import org.eclipse.ui.IViewPart;
+import org.eclipse.ui.IWorkbenchPart;
+
+public class AddToFavoritesViewActionDelegate implements IViewActionDelegate {
+
+	private IWorkbenchPart targetPart;
+
+	@Override
+	public void run(IAction action) {
+		MessageDialog.openInformation(targetPart.getSite().getShell(), "Add to Favorites",
+				"Triggered the " + getClass().getName() + " action");
+
+	}
+
+	@Override
+	public void selectionChanged(IAction action, ISelection selection) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void init(IViewPart view) {
+		this.targetPart = view;
+
+	}
+
+}
