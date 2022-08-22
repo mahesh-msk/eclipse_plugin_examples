c89fb2f856791797e0759d739a6bece1d0fee777
m.maheshbhagirath@gmail.com <Mahesh>
2022-08-22 11:29:28 +0530

    Builder,nature and Markers

---
+++ com.ancit.markers/src/com/ancit/markers/builder/AddRemoveSampleNatureHandler.java
@@ -0,0 +1,84 @@
+/*
+ * FILE:            AddRemoveSampleNatureHandler.java
+ *
+ * SW-COMPONENT:    com.ancit.markers
+ *
+ * DESCRIPTION:     -
+ *
+ * COPYRIGHT:       © 2015 - 2022 Robert Bosch GmbH
+ *
+ * The reproduction, distribution and utilization of this file as
+ * well as the communication of its contents to others without express
+ * authorization is prohibited. Offenders will be held liable for the
+ * payment of damages. All rights reserved in the event of the grant
+ * of a patent, utility model or design.
+ */
+package com.ancit.markers.builder;
+
+import org.eclipse.core.commands.AbstractHandler;
+import org.eclipse.core.commands.ExecutionEvent;
+import org.eclipse.core.commands.ExecutionException;
+import org.eclipse.core.resources.IProject;
+import org.eclipse.core.resources.IProjectDescription;
+import org.eclipse.core.runtime.CoreException;
+import org.eclipse.core.runtime.IAdaptable;
+import org.eclipse.jface.viewers.ISelection;
+import org.eclipse.jface.viewers.IStructuredSelection;
+import org.eclipse.ui.handlers.HandlerUtil;
+
+public class AddRemoveSampleNatureHandler extends AbstractHandler {
+
+	@Override
+	public Object execute(ExecutionEvent event) throws ExecutionException {
+		ISelection selection = HandlerUtil.getCurrentSelection(event);
+		//
+		if (selection instanceof IStructuredSelection) {
+			for (Object element : ((IStructuredSelection) selection)) {
+				IProject project = null;
+				if (element instanceof IProject) {
+					project = (IProject) element;
+				} else if (element instanceof IAdaptable) {
+					project = ((IAdaptable) element).getAdapter(IProject.class);
+				}
+				if (project != null) {
+					try {
+						toggleNature(project);
+					} catch (CoreException e) {
+						throw new ExecutionException("Failed to toggle nature", e);
+					}
+				}
+			}
+		}
+
+		return null;
+	}
+
+	/**
+	 * Toggles sample nature on a project
+	 *
+	 * @param project to have sample nature added or removed
+	 */
+	private void toggleNature(IProject project) throws CoreException {
+		IProjectDescription description = project.getDescription();
+		String[] natures = description.getNatureIds();
+
+		for (int i = 0; i < natures.length; ++i) {
+			if (SampleNature.NATURE_ID.equals(natures[i])) {
+				// Remove the nature
+				String[] newNatures = new String[natures.length - 1];
+				System.arraycopy(natures, 0, newNatures, 0, i);
+				System.arraycopy(natures, i + 1, newNatures, i, natures.length - i - 1);
+				description.setNatureIds(newNatures);
+				project.setDescription(description, null);
+				return;
+			}
+		}
+
+		// Add the nature
+		String[] newNatures = new String[natures.length + 1];
+		System.arraycopy(natures, 0, newNatures, 0, natures.length);
+		newNatures[natures.length] = SampleNature.NATURE_ID;
+		description.setNatureIds(newNatures);
+		project.setDescription(description, null);
+	}
+}
